f7a45708c0ab29fb4ee2bae2299216785c006f96
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-05 14:14:36 +0200

    change project structure for easier testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
-use axum::{Router, http::StatusCode, routing::get};
+use zero2prod_axum::run;
 
-async fn greet() -> &'static str {
-    "Hello, World!\n"
-}
-
-async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new()
-        .route("/", get(greet))
-        .route("/health_check", get(health_check));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+    run().await;
 }
