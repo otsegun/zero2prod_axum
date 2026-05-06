6cdc0ac8aed2b5946eb0895b3547728d130e1946
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-06 07:53:42 +0200

    feat: add tests for parsing forms

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use zero2prod_axum::{app, run};
+use zero2prod_axum::run;
 
 #[tokio::main]
-// async fn main() -> () {
-//     // TODO(segun): main doesn't block anymore, fix this.
-//     let _addr = run().await;
-//     // format!("http://{}", addr)
-// }
-
 async fn main() -> Result<(), std::io::Error> {
     run().await?;
     Ok(())
