d010a402377a53e4363469389f219c55e16b2dfd
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-22 11:46:03 +0200

    Migrating from log to tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,13 +13,13 @@ pub async fn subscribe(State(state): State<PgPool>, Form(form_data): Form<FormDa
     //Generate a random unique identifier
     let request_id = Uuid::new_v4();
     //Log subscriber email and name
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form_data.email,
         form_data.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscirber details in the database",
         request_id
     );
@@ -37,14 +37,14 @@ pub async fn subscribe(State(state): State<PgPool>, Form(form_data): Form<FormDa
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
             StatusCode::OK
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e
