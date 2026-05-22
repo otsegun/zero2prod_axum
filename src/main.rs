44991329faa2ed7e0ef8db77158b092ce344a969
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-22 11:07:28 +0200

    The Facade pattern: Axum doesn't have a prebuilt middleware like Actix's Logger::default()  so I couldn't a log for  every request, but still using trace level in env_logger  initialization, we're able to see the trace logs from axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod_axum::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Setup env_logger: `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
 
