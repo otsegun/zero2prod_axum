b913f34d3c0fcb9c786e769dcfbffcd6e72d08d0
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-22 11:22:07 +0200

    Think like a user

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,13 @@ pub struct FormData {
 }
 
 pub async fn subscribe(State(state): State<PgPool>, Form(form_data): Form<FormData>) -> StatusCode {
+    //Log subscriber email and name
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.",
+        form_data.email,
+        form_data.name
+    );
+    log::info!("Saving new subscirber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +30,12 @@ pub async fn subscribe(State(state): State<PgPool>, Form(form_data): Form<FormDa
     .execute(&state)
     .await
     {
-        Ok(_) => StatusCode::OK,
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            StatusCode::OK
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
