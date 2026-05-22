9688ce7cb79557c3f9659603246f1fbea3113688
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-22 14:33:11 +0200

    tracing-bunyan-formatter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod_axum::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Setup env_logger: `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
+    // We are falling back to printing all spans at info-level or above
     // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+    // env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("trace"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // Output the formatted spans to stdout
+        std::io::stdout,
+    );
+
+    // The `with` method is provided by `SubscriberExt`, an extension
+    // trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // `set_global_default` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
